944de41f160342dd792e492c6a00c45f4a6d7f8b
Daochien <chiendaovan@muare.vn>
2018-10-31 17:53:12 +0700

    no message

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -1,3 +1,18 @@
-var video = require('./playvideo');
+const wrtc = require('electron-webrtc')
+const Peer = require('simple-peer');
+const p = new Peer({initiator: location.hash === "#1", trickle: false});
+p.on('signal', token => {
+    $("#token_stream_teacher").val(JSON.stringify(token));
+});
 
-video();
+$("body").on('click', '#connect_class', function(){
+    const friendSignal = JSON.parse($('#get_token_stream').val());
+    console.log(friendSignal);
+    p.signal(friendSignal);
+});
+
+p.on('connect', function(){
+    console.log('Đã connect giáo viên vs học viên');
+});
+
+console.log('xin chao cac ban');
